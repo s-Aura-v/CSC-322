c11c83f6427632101916e78ea8076a4fcfceb07f
s-Aura-v <slamich2@oswego.edu>
2023-10-24 21:37:25 -0400

    Style + Lab3 Structure

---
+++ cacheProject_Lab3/cache.c
@@ -0,0 +1,30 @@
+//
+// Created by Saurav Lamichhane on 10/24/23.
+//
+
+#include <stdbool.h>
+#include <printf.h>
+
+int main() {
+
+    //Necessary variables
+    int s;
+    int e;
+    int b;
+    int m;
+    char policy;
+    int hitTime;
+    int missPenalty;
+
+    //Initialize the variables
+        scanf("Enter the number of sets (S): %d \n", &s);
+        scanf("Enter the number of lines per set (E): %d", &e);
+        scanf("Enter the block size B: %d", &b);
+        scanf("Enter the number of physical memory bits (m): %d", &m);
+        scanf("Choose a policy: [L : %d", &s);
+        scanf("Enter the hit time : %d", &hitTime);
+        scanf("Enter the miss penality : %d", &missPenalty);
+
+    }
+
+}
