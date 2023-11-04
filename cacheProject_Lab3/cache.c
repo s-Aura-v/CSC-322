3893b91cab28ba9e304cb48caf7983d60bfad3ff
s-Aura-v <slamich2@oswego.edu>
2023-11-03 23:21:35 -0400

    Bug Fixes
    1. Fixed 2d array malloc
    2. updated hexToBinary

--- cacheProject_Lab3/cache.c
+++ cacheProject_Lab3/cache.c
@@ -28,8 +28,6 @@ int hitTime = 0;
 int missPenalty = 0;
 char* binaryAddress;
 
-
-
 //Prototypes
 void cacheSimulation(CacheLine **cache, int S, int E, int B, int m, int s, int b, int t, char input[]);
 void hexToBinary(char memoryAddress[]);
@@ -69,20 +67,20 @@ int main() {
     int t = m - (b + s);
 
     //Create the cache
-    CacheLine ** cache = malloc(S * sizeof(CacheLine **));
-    for (int i = 0; i < E; i++) {
-        cache = malloc(E * sizeof(CacheLine **));
+    CacheLine ** cache = malloc(S * sizeof(CacheLine *));
+    for (int i = 0; i < S; i++) {
+        cache[i] = malloc(E * sizeof(CacheLine *));
     }
-    //BROKEN CODE
-//    //Set Variable
-//    for (int i = 0; i < S; i++) {
-//        for (int j = 0; j < E; j++) {
-//            cache[i][j].valid = 0;
-//            cache[i][j].tag = 0;
-//            cache[i][j].amntUsed = 0;
-//            cache[i][j].cycleCounter = 0;
-//        }
-//    }
+
+    //Set Variable
+    for (int i = 0; i < S; i++) {
+        for (int j = 0; j < E; j++) {
+            cache[i][j].valid = 0;
+            cache[i][j].tag = 0;
+            cache[i][j].amntUsed = 0;
+            cache[i][j].cycleCounter = 0;
+        }
+    }
 
     //Get the inputs
     char input[64];
@@ -103,44 +101,36 @@ void cacheSimulation(CacheLine **cache, int S, int E, int B, int m, int s, int b
 
     //2. Get Set ID: complete
     char *setID = malloc(20 * sizeof(char *));  //20 is temporary value for now
-    //start from substring and go to the end and set it as setID
-    int binaryEnd = strlen(binaryAddress) - 1 - b;
     strncpy(setID, binaryAddress + t, s);
     printf("SetID: %s\n", setID);
 
     //3. Tag Number: complete
     char *tagNum = malloc(50 * sizeof(char *)); //50 temp
-    binaryEnd = strlen(binaryAddress) - (s + b);
+    int binaryEnd = strlen(binaryAddress) - (s + b);
     strncpy(tagNum, binaryAddress, binaryEnd);
     printf("tagNum: %s\n", tagNum);
 
-
-
-    printf("BinAdd: %s\n", binaryAddress);
-    printf("TempAdd: %s", setID);
-
     //4. Do the simulation
-
     bool complete = false;
     int currentCycle = 0;
 
-//    while (complete == false) {
-//        bool lineExist = false;
-//        totalCycles += hitTime;
-//        totalCycles += 1;
-//
-//        for (int i = 0; i < S; i++) {
-//            if (lineExist == true) {
-//                break;
-//            }
+    while (complete == false) {
+        bool lineExist = false;
+        totalCycles += hitTime;
+        totalCycles += 1;
+
+        for (int i = 0; i < S; i++) {
+            if (lineExist == true) {
+                break;
+            }
 //            if (cache[][] == setID)
-//
-//
-//        }
+
+
+        }
 
         complete = true;
     }
-    //Body
+}
 
 
 void hexToBinary(char memoryAddress[]) {
@@ -178,21 +168,27 @@ void hexToBinary(char memoryAddress[]) {
                 strcat(binaryAddress, "1001");
                 break;
             case 'a':
+            case 'A':
                 strcat(binaryAddress, "1010");
                 break;
             case 'b':
+            case 'B':
                 strcat(binaryAddress, "1011");
                 break;
             case 'c':
+            case 'C':
                 strcat(binaryAddress, "1100");
                 break;
             case 'd':
+            case 'D':
                 strcat(binaryAddress, "1101");
                 break;
             case 'e':
+            case 'E':
                 strcat(binaryAddress, "1110");
                 break;
             case 'f':
+            case 'F':
                 strcat(binaryAddress, "1111");
                 break;
         }
