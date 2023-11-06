30f3e60a67197d3fbd0205c087056466b6b18913
s-Aura-v <slamich2@oswego.edu>
2023-11-05 22:15:49 -0500

    Cache Project Complete
    1. No memory leaks
    2. No errors
    3. Removed extraneous code

--- cacheProject_Lab3/cache.c
+++ cacheProject_Lab3/cache.c
@@ -12,8 +12,8 @@ typedef struct {
     int valid;
     int tag;
     int setNumber;
-    int amntUsed;              //LFU requires you to remove the value that is used the least
-    int cycleCounter;           //LRU requires you to remove the value that is the oldest in the timeline
+    int amntUsed;              //LFU - least accessed
+    int cycleCounter;           //LRU - oldest in timeline
 } CacheLine;
 
 //Globals
@@ -27,10 +27,8 @@ char* binaryAddress;
 char setID[64];
 char tagStr[64];
 
-
-
 //Prototypes
-void cacheSimulation(CacheLine **cache, int S, int E, int B, int m, int s, int b, int t, char input[]);
+void cacheSimulation(CacheLine **cache, int S, int E, int s, int b, int t, char input[]);
 void hexToBinary(char memoryAddress[]);
 int binaryToInt(char* setID);
 
@@ -85,19 +83,13 @@ int main() {
     }
 
     //Get the inputs
-    char * input;
-    input = malloc(65 * sizeof (char));
-    scanf("%s", input);
-    int i = 0;
+    char input[65];
+    strcpy(input, "placeholder");
     while (atoi(input) != -1) {
-        if (i != 0) {
-            input = malloc(64 * sizeof(char));
-            scanf("%s", input);
-        }
+        scanf("%s", input);
         if (atoi(input) != -1) {
-            cacheSimulation(cache, S, E, B, m, s, b, t, input);
+            cacheSimulation(cache, S, E, s, b, t, input);
         }
-        i++;
     }
 
     //Final Calculations:
@@ -109,10 +101,9 @@ int main() {
         free(cache[i]);
     }
     free(cache);
-    free(input);
 }
 
-void cacheSimulation(CacheLine **cache, int S, int E, int B, int m, int s, int b, int t, char input[]) {
+void cacheSimulation(CacheLine **cache, int S, int E, int s, int b, int t, char input[]) {
     binaryAddress = calloc(260, sizeof (char*));
     binaryAddress[0] = '\0';
 
@@ -120,154 +111,136 @@ void cacheSimulation(CacheLine **cache, int S, int E, int B, int m, int s, int b
     hexToBinary(input);
 
     //2. Get Set ID: complete
-//    char *setID = malloc(20 * sizeof(char *));  //20 is temporary value for now
     strncpy(setID, binaryAddress + t, s);
     int setNum = binaryToInt(setID);
 
     //3. Tag Number: complete
-//    char *tagStr = malloc(50 * sizeof(char *)); //50 temp
     int binaryEnd = strlen(binaryAddress) - (s + b);
     strncpy(tagStr, binaryAddress, binaryEnd);
     int tagNum = binaryToInt(tagStr);
 
-//    //Debug
-//    printf("%s\n", binaryAddress);
-//    printf("SetID: %s\n", setID);
-//    printf("SetNum: %d\n", setNum);
-//    printf("tagStr: %s\n", tagStr);
-//    printf("tagNum: %d\n", tagNum);
-
     //4. Do the simulation
     bool complete = false;
+    totalRuns += 1;
 
-    while (complete == false) {
-        totalRuns += 1;
+    //Check if bits/tags exists
+    bool validBitExists = false;
+    for (int i = 0; i < S; i++) {
+        for (int j = 0; j < E; j++) {
+            //No need to continue after finding a hit;
+            //1. Assuming it is in the cache: ValidBit exists and TagExists
+            if (cache[i][j].setNumber == setNum) {
+                if (cache[i][j].valid == 1) {
+                    validBitExists = true;
+                    if (cache[i][j].tag == tagNum) {
+                        totalCycles += hitTime;
+                        cache[i][j].cycleCounter = totalCycles;
 
-        //Check if bits/tags exists
-        bool validBitExists = false;
-        bool tagExists = false;
+                        printf("%s H\n", input);
+                        complete = true;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    //1: Assume that validBit does not exist.
+    bool roomAdded = false;
+    if (complete == false) {
+        if (validBitExists == false) {
+            for (int i = 0; i < S; i++) {
+                for (int j = 0; j < E; j++) {
+                    if (cache[i][j].setNumber == setNum) {
+                        //1. There is room for the address
+                        if (cache[i][j].valid == 0 && roomAdded == false) {
+                            totalMiss++;
+                            totalCycles += missPenalty + hitTime;
+
+                            cache[i][j].valid = 1;
+                            cache[i][j].tag = tagNum;
+                            cache[i][j].cycleCounter = totalCycles;
+                            cache[i][j].amntUsed++;
+
+                            printf("%s M\n", input);
+                            roomAdded = true;
+                            complete = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //2. Assume that validBit does exist and the tag does not match. Evict!
+    roomAdded = false;
+    //a. Find the least used
+    if (complete == false) {
+        int leastUsed = 0;
         for (int i = 0; i < S; i++) {
             for (int j = 0; j < E; j++) {
-                //No need to continue after finding a hit;
-                //1. Assuming it is in the cache: ValidBit exists and TagExists
                 if (cache[i][j].setNumber == setNum) {
-                    if (cache[i][j].valid == 1) {
-                        validBitExists = true;
-                        if (cache[i][j].tag == tagNum) {
-                            totalCycles += hitTime;
+                    leastUsed = cache[i][j].amntUsed;
+                    int challenger = cache[i][j].amntUsed;
+                    if (challenger < leastUsed) {
+                        leastUsed = challenger;
+                    }
+                }
+            }
+        }
+        //b. Find the oldest in the time-line
+        int leastCycles = 0;
+        for (int i = 0; i < S; i++) {
+            for (int j = 0; j < E; j++) {
+                if (cache[i][j].setNumber == setNum) {
+                    leastCycles = cache[i][j].cycleCounter;
+                    int challenger = cache[i][j].cycleCounter;
+                    if (challenger < leastCycles) {
+                        leastCycles = challenger;
+                    }
+                }
+            }
+        }
+        //c. Compare and Evict!
+        for (int i = 0; i < S; i++) {
+            for (int j = 0; j < E; j++) {
+                if (policy == 'F') {
+                    if (cache[i][j].setNumber == setNum) {
+                        if (cache[i][j].amntUsed == leastUsed && roomAdded == false) {
+                            //evict!
+                            totalMiss++;
+                            totalCycles += missPenalty + hitTime;
+
+                            cache[i][j].tag = tagNum;
                             cache[i][j].cycleCounter = totalCycles;
 
-                            printf("%s H\n", input);
-                            tagExists = true;
+                            printf("%s M\n", input);
+                            roomAdded = true;
                             complete = true;
-                            break;
                         }
                     }
-                }
-            }
-        }
-
-        //1: Assume that validBit does not exist.
-        bool roomAdded = false;
-        if (complete == false) {
-            if (validBitExists == false) {
-                for (int i = 0; i < S; i++) {
-                    for (int j = 0; j < E; j++) {
-                        if (cache[i][j].setNumber == setNum) {
-                            //1. There is room for the address
-                            if (cache[i][j].valid == 0 && roomAdded == false) {
-                                totalMiss++;
-                                totalCycles += missPenalty + hitTime;
-
-                                cache[i][j].valid = 1;
-                                cache[i][j].tag = tagNum;
-                                cache[i][j].cycleCounter = totalCycles;
-                                cache[i][j].amntUsed++;
-
-                                printf("%s M\n", input);
-                                roomAdded = true;
-                                complete = true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        //2. Assume that validBit does exist and the tag does not match. Evict!
-        roomAdded = false;
-        //a. Find the least used
-        if (complete == false) {
-            int leastUsed = 0;
-            for (int i = 0; i < S; i++) {
-                for (int j = 0; j < E; j++) {
+                } else { //LRU
                     if (cache[i][j].setNumber == setNum) {
-                        leastUsed = cache[i][j].amntUsed;
-                        int challenger = cache[i][j].amntUsed;
-                        if (challenger < leastUsed) {
-                            leastUsed = challenger;
-                        }
-                    }
-                }
-            }
-            //b. Find the oldest in the time-line
-            int leastCycles = 0;
-            for (int i = 0; i < S; i++) {
-                for (int j = 0; j < E; j++) {
-                    if (cache[i][j].setNumber == setNum) {
-                        leastCycles = cache[i][j].cycleCounter;
-                        int challenger = cache[i][j].cycleCounter;
-                        if (challenger < leastCycles) {
-                            leastCycles = challenger;
-                        }
-                    }
-                }
-            }
-            //c. Compare and Evict!
-            for (int i = 0; i < S; i++) {
-                for (int j = 0; j < E; j++) {
-                    if (policy == 'F') {
-                        if (cache[i][j].setNumber == setNum) {
-                            if (cache[i][j].amntUsed == leastUsed && roomAdded == false) {
-                                //evict!
-                                totalMiss++;
-                                totalCycles += missPenalty + hitTime;
+                        if (cache[i][j].cycleCounter == leastCycles && roomAdded == false) {
+                            //evict!
 
-                                cache[i][j].tag = tagNum;
-                                cache[i][j].cycleCounter = totalCycles;
+                            cache[i][j].tag = tagNum;
+                            cache[i][j].cycleCounter = totalCycles;
 
-                                printf("%s M\n", input);
-                                roomAdded = true;
-                                complete = true;
-                            }
-                        }
-                    } else { //LRU
-                        if (cache[i][j].setNumber == setNum) {
-                            if (cache[i][j].cycleCounter == leastCycles && roomAdded == false) {
-                                //evict!
+                            totalMiss++;
+                            totalCycles += missPenalty + hitTime;
 
-                                cache[i][j].tag = tagNum;
-                                cache[i][j].cycleCounter = totalCycles;
-
-                                totalMiss++;
-                                totalCycles += missPenalty + hitTime;
-
-                                printf("%s M\n", input);
-                                roomAdded = true;
-                                complete = true;
-                            }
+                            printf("%s M\n", input);
+                            roomAdded = true;
+                            complete = true;
                         }
                     }
                 }
             }
         }
-        //End the simulation
-        free(input);
-        free(binaryAddress);
-//        free(setID);
-//        free(tagStr);
-        complete = true;
     }
+    //End the simulation
+    free(binaryAddress);
 }
 
 void hexToBinary(char memoryAddress[]) {
@@ -332,9 +305,6 @@ void hexToBinary(char memoryAddress[]) {
 }
 
 int binaryToInt(char* setID) {
-    if (setID != NULL) {
-        int binary = (int) strtol(setID, NULL, 2);
-        return binary;
-    }
-    return -1;
+    int binary = (int) strtol(setID, NULL, 2);
+    return binary;
 }
